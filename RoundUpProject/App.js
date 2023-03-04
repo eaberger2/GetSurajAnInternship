90a4d03cb1f0870726f3a5d8cfe6fa5b67515e4e
eaberger2 <eaberger2@wisc.edu>
2023-03-04 16:22:19 -0600

    tab bar

--- RoundUpProject/App.js
+++ RoundUpProject/App.js
@@ -1,38 +1,28 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Transactions } from './Transactions';
+import { Categories } from './Categories';
 
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-
-export default function App() {
+function Accounts() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Account Manager</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Overview of Accounts" onPress={() => console.log('Overview pressed')} />
-        <Button title="Edit Accounts" onPress={() => console.log('Edit pressed')} />
-        <Button title="View Accounts" onPress={() => console.log('View pressed')} />
-        <Button title="Create New" onPress={() => console.log('Create pressed')} />
-        <Button title="Total Saved" onPress={() => console.log('Total Saved pressed')} />
-      </View>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Accounts</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  buttonContainer: {
-    marginTop: 32,
-    width: '80%',
-  },
-});
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Accounts" component={Accounts} />
+        <Tab.Screen name="Transactions" component={Transactions} />
+        <Tab.Screen name="Categories" component={Categories} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
