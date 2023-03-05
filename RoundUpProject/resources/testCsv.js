d20ea9345ca9520ebc840c68dfdb31c8fdd329c4
bigPhishy <126978921+bigPhishy@users.noreply.github.com>
2023-03-05 09:36:13 -0600

    I am making an object of hashMapMaker.

--- RoundUpProject/resources/testCsv.js
+++ RoundUpProject/resources/testCsv.js
@@ -1,23 +1,7 @@
-const fs = require('fs');
-import { parse } from '@vanillaes/csv'
-function test (){
-fs.readFile('./companies.csv', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+import HashMapMaker from './HashMapMaker.js';
 
-  const rows = data.split('\n');
-  const headers = rows[2].split(',');
-  const obj = {};
-
-  for (let i = 0; i < headers.length; i++) {
-    obj[headers[i]] = null;
-  }
-
-  console.log(obj);
-});
-}
+var myMaker = new HashMapMaker();
+myMaker.makeHash();
 
 
 
