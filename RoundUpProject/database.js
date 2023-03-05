1dc316e2813aabc33f353cd96e85b225457746d9
Your Name <surajsomc@gmail.com>
2023-03-04 21:19:17 -0600

    change database

--- RoundUpProject/database.js
+++ RoundUpProject/database.js
@@ -8,38 +8,21 @@ const client = new Client({
     database: "initial_db"
 })
 
-client.connect((err) => {
-    //Checks if the connection is successful
-    if (err) {
-      console.error('connection error', err.stack)
-      client.end();
-    } else {
-      // Since the client connects to the database now you can read the database
-      console.log('connected')
 
-      //Printing out the value of the table in the console
-      client.query('SELECT * FROM account', (err, res) => {
-        if (err){
-            console.log(err.message);
-            client.end();
-        }else{
-            console.table(res.rows);
-        }
-        
-        //Inserting the specified account value into the database
-        /*client.query('insert into account values ($1, $2)', [200, 'Stewart'], (err, res) => {
-            if (err){
-                console.log(err.message);
-                client.end();
-            }else{
-                console.table(res.rows);
-            }
-        })*/
+execute()
 
+async function execute(){
+    try{
+        await client.connect()
+        console.log("Connected successfully.")
+        //await client.query("insert into account values (500, 'John')")
 
-    });
-
+        const {rows} = await client.query("select * from account")
+        console.table(rows)
+    }catch (ex){
+        console.log(`Something wrong happened ${ex}`)
+    }finally{
+        await client.end()
+        console.log("Client disconnected successfully.")
     }
-  })
-
-
+}
