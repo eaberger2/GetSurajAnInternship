49516823110f5205217706503a9f1dc0d9bb24af
ClearCrystals <gesparkles@gmail.com>
2023-03-04 18:56:03 -0600

    Form Working

---
+++ RoundUpProject/components/AccountCreation.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+export function AccountCreation() {
+    const [currency, setCurrency] = useState('US Dollar');
+    return (
+        <View >
+          <Text > Demo Form </Text>
+          <View>
+            <TextInput 
+              placeholder="Email" />
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Password"
+            />
+            <Picker
+              selectedValue={currency}
+              onValueChange={currentCurrency => setCurrency(currentCurrency)}>
+              <Picker.Item label="USD" value="US Dollars" />
+              <Picker.Item label="EUR" value="Euro" />
+              <Picker.Item label="NGN" value="Naira" />
+            </Picker>
+            <Text>
+              Selected: {currency}
+            </Text>
+          </View>
+        </View>
+      );
+}
