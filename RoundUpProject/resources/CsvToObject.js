2ea705c6be101007cf53e3803452e384810e38b3
ClearCrystals <gesparkles@gmail.com>
2023-03-04 21:01:04 -0600

    notes update

--- RoundUpProject/resources/CsvToObject.js
+++ RoundUpProject/resources/CsvToObject.js
@@ -1,27 +1,16 @@
-var csv = require('jquery-csv');
-var fs = require('fs');
-const sample = './companies.csv';
-const map1 = new Map();
-map1.set("Hello", 123)
-console.log(map1.size)
-console.log(map1.get("Hello"))
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
 
-testName = ""
-fs.readFile(sample, 'UTF-8', (err, fileContent) => {
-  if (err) { console.log(err); }
-  csv.toObjects(fileContent, {}, (err, data) => {
-    if (err) { console.log(err); }
-    // let myArray = [data.length];
-    // let myArray2 = [data.length];
-    for (let i = 0, len = data.length; i < len; i++) {
-    //    myArray.push(data[i].name)
-    //    myArray2.push(data[i].market)
-    }
-  });
-});
+const csvData = fs.readFileSync('companies.csv');
+const records = parse(csvData, { columns: true });
 
-// for (let k = 0, len = myArray.length; k++) {
-//     map1.set(myArray, myArray2)
-// }
-// console.log(map1.has(data[1].name))
-console.log(map1.size)
+const hashmap = {};
+for (let i = 0; i < records.length; i++) {
+  const record = records[i];
+  const name = record.name;
+  const category = record.category_list;
+  const countryCode = record.country_code;
+  hashmap[name] = { category, countryCode };
+}
+
+console.log(hashmap);
