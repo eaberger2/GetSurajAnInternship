9eb2b92a7b912f457289bc440967b8e199e7a41a
bigPhishy <126978921+bigPhishy@users.noreply.github.com>
2023-03-04 21:19:44 -0600

    Added the hashmap

--- RoundUpProject/resources/CsvToObject.js
+++ RoundUpProject/resources/CsvToObject.js
@@ -1,27 +1,42 @@
-var csv = require('jquery-csv');
 var fs = require('fs');
+var csv = require('jquery-csv');
 const sample = './companies.csv';
 const map1 = new Map();
 map1.set("Hello", 123)
 console.log(map1.size)
 console.log(map1.get("Hello"))
 
-testName = ""
+// fs.readFile(sample, 'UTF-8', function (err, csv) {
+//     if (err) { console.log(err); }
+//     csv.toArrays(csv, {}, function (err, data) {
+//       if (err) { console.log(err); }
+//       for (var i = 0, len = data.length; i < len; i++) {
+//         console.log(data[i]);
+//       }
+//     });
+//   });
+testName = "bob"
+const myArray = [];
+const myArray2 = [];
 fs.readFile(sample, 'UTF-8', (err, fileContent) => {
   if (err) { console.log(err); }
   csv.toObjects(fileContent, {}, (err, data) => {
     if (err) { console.log(err); }
-    // let myArray = [data.length];
-    // let myArray2 = [data.length];
     for (let i = 0, len = data.length; i < len; i++) {
-    //    myArray.push(data[i].name)
+        map1.set(data[i].name, data[i].market)
+        // console.log(data[i])
+        // testName = data[0].name
+        // myArray.push(testName)
     //    myArray2.push(data[i].market)
     }
+    console.log("map size after loop:" + map1.size)
   });
 });
 
-// for (let k = 0, len = myArray.length; k++) {
+// for (let k = 0, len = myArray.length; k < len; k++) {
 //     map1.set(myArray, myArray2)
 // }
 // console.log(map1.has(data[1].name))
-console.log(map1.size)
+map1.set(testName, 000)
+console.log("map1 size: " + map1.size)
+console.log("myArray length:" + myArray.length)
