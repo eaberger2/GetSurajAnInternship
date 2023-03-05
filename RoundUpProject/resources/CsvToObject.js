0f8a64af2cf2edc11661a5386b6332d8ad93e352
bigPhishy <126978921+bigPhishy@users.noreply.github.com>
2023-03-04 23:55:05 -0600

    Finished hashmapper

--- RoundUpProject/resources/CsvToObject.js
+++ RoundUpProject/resources/CsvToObject.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var csv = require('jquery-csv');
-const sample = './companies.csv';
+const sample = './companies1.csv';
 // const map1 = new Map();
 // map1.set("Hello", 123)
 // console.log(map1.size)
@@ -16,7 +16,7 @@ fs.readFile(sample, 'UTF-8', (err, fileContent) => {
     if (err) { console.log(err); }
 
     for (let i = 0, len = data.length; i < len; i++) {
-        map1.set(data[i].name, data[i].market)
+        map1.set(data[i].Name, data[i].Sector)
     }
 
     console.log("finished loop:" + map1.size)
@@ -30,67 +30,6 @@ fs.readFile(sample, 'UTF-8', (err, fileContent) => {
 
 p.then((map1) => {
     console.log("passed")
-    console.log(map1.get('Club Domains'))
+    console.log(map1.get('Aflac'))
 });
-// getActivity()
-// async function getActivity(){
-//     var response = makeHashMap();
-//     // console.log("map1 size: " + map1.size)
-//     console.log(response.get("Club Domains"))
-//     // console.log(map1.get("Club Domains"))
-    // console.log(map1.has("Club Domains"))
-// }
-// console.log(map1.has("Club Domains"))
 
-
-// awaitconsole.log(map1.get("McDonalds"))
-
-// async function myAsyncFunction() {
-//     let result = await myPromise();
-//     console.log(result);
-//   }
-  
-//   function myPromise() {
-//     return new Promise(resolve => {
-//       setTimeout(() => {
-//         resolve("Hello, world!");
-//       }, 2000);
-//     });
-//   }
-  
-//   myAsyncFunction();
-
-// testInput = './testInputTransactions.csv'
-// fs.readFile(testInput, 'UTF-8', (err, fileContent) => {
-//     if (err) { console.log(err); }
-//     csv.toObjects(fileContent, {}, (err, data) => {
-//         if (err) { console.log(err); }
-//         for (let i = 0, len = data.length; i < len; i++) {
-//             console.log(map1.get(data[i].name))
-//         }
-//     });
-// });
-
-// for (let k = 0, len = myArray.length; k < len; k++) {
-//     map1.set(myArray, myArray2)
-// }
-// console.log(map1.has(data[1].name))
-
-// =======
-// const fs = require('fs');
-// const parse = require('csv-parse/lib/sync');
-
-// const csvData = fs.readFileSync('companies.csv');
-// const records = parse(csvData, { columns: true });
-
-// const hashmap = {};
-// for (let i = 0; i < records.length; i++) {
-//   const record = records[i];
-//   const name = record.name;
-//   const category = record.category_list;
-//   const countryCode = record.country_code;
-//   hashmap[name] = { category, countryCode };
-// }
-
-// console.log(hashmap);
-// >>>>>>> 1dc316e2813aabc33f353cd96e85b225457746d9
