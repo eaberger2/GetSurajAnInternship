66d49dbb92fe929d893673494b4c02217dc1c259
bigPhishy <126978921+bigPhishy@users.noreply.github.com>
2023-03-05 09:58:59 -0600

    tinkering with HashMapMaker object

--- RoundUpProject/resources/testCsv.js
+++ RoundUpProject/resources/testCsv.js
@@ -1,7 +1,10 @@
 import HashMapMaker from './HashMapMaker.js';
+import {makeHash} from './HashMapMaker.js';
+import {recieveHash} from './HashMapMaker.js';
 
 var myMaker = new HashMapMaker();
 myMaker.makeHash();
+myMaker.receiveHash()
 
 
 
